685cbb4ced200f3b43dfaa35ef1006d0dc2a316b
Triston Land <tristonland@gmail.com>
2025-07-09 00:38:02 -0600

    change use state to object with champ info and url

--- src/MemoryCard.jsx
+++ src/MemoryCard.jsx
@@ -1,26 +1,43 @@
 import { useEffect, useState } from "react";
+import ChampionJson from "./assets/champion.json";
+const key = import.meta.env.VITE_API_KEY;
+const LEAGUEVERSION = "15.13.1";
+
+function getChamp() {
+  let champName = Object.keys(ChampionJson.data)[
+    Math.round(Math.random() * Object.keys(ChampionJson.data).length)
+  ];
+  let champImage = ChampionJson.data[champName].image.full;
+  return [champName, champImage];
+}
+
+function addSeenChamp() {}
 
 async function getImage() {
-  let aatroxImage = await fetch(
-    "https://ddragon.leagueoflegends.com/cdn/img/champion/loading/Aatrox_0.jpg"
+  let [champName, champImage] = getChamp();
+  let Image = await fetch(
+    `https://ddragon.leagueoflegends.com/cdn/${LEAGUEVERSION}/img/champion/${champImage}`
   );
-
-  console.log(aatroxImage.url);
-  return aatroxImage.url;
+  Math.round(Math.random() * Object.keys(ChampionJson.data));
+  return [champName, Image.url];
 }
 
 function MemoryCard() {
-  const [aatroxImage, setimage] = useState(null);
+  const [info, setInfo] = useState({ name: null, image: null });
+  console.log(info);
   useEffect(() => {
-    getImage().then(setimage);
-  });
+    getImage().then((champ) => {
+      setInfo(() => ({ name: champ[0], image: champ[1] }));
+    });
+  }, []);
+
   return (
     <>
       <div>hello</div>
       <div>Picture Area</div>
       <div className="pictureContainer">
         <div className="picture">
-          <img src={aatroxImage} alt="" />
+          <img src={info.image} alt="" />
         </div>
       </div>
     </>
